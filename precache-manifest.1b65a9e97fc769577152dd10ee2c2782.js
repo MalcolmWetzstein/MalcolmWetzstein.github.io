a27a962b4857a3c2e19687085ab8ec5f53b991ae
Malcolm Wetzstein <mxw002@gmail.com>
2019-12-17 00:18:29 -1000

    Updates

---
+++ precache-manifest.1b65a9e97fc769577152dd10ee2c2782.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a1ac792d773df656dc1cacbc92a8354",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e08b74b0d855ceddce9",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "510aeafd5b31554d6cc6",
+    "url": "/static/js/2.5f8f5a35.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5f8f5a35.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e08b74b0d855ceddce9",
+    "url": "/static/js/main.2b215e11.chunk.js"
+  },
+  {
+    "revision": "b48494abca701cfe4673",
+    "url": "/static/js/runtime-main.6271eae4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
