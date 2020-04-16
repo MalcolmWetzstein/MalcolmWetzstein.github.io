b6bbb7818cc415f0fd94365ce6e8a9ff16323190
Malcolm Wetzstein <mxw002@gmail.com>
2020-04-16 01:48:37 -1000

    Adaptive manifolds text content

--- src/pages/portfolio/AdaptiveFilter.js
+++ src/pages/portfolio/AdaptiveFilter.js
@@ -35,9 +35,45 @@ class AdaptiveFilter extends CustomComponent
                 </Container>
                 <Space size='sm'/>
                 <Typography>
-                    {/* Explaination */}
+                    Simple image filters like the Gaussian Blur operate in two dimensions, the x and y dimensions of the image. These kinds of filters are called spatial, or linear, 
+                    filters; a pre-computation step can be used to speed up evaluation of the filter. High-dimensional, or non-linear, filters operate in three or more dimensions on 
+                    images, where each additional dimension is associated with a pixel value. The bilateral filter is a 5D filter where RGB color values correspond to the three 
+                    additional dimensions. Because they depend on values at each pixel, pre-computation cannot be used to speed up evaluation of the filter and the entire image must 
+                    be looped over once per pixel in order to evaluate the filter at that pixel.
                 </Typography>
-                {/* Images of demos with captions (manifold tree, blurring, denoising, depth of field) */}
+                <Space size='xs'/>
+                <Typography>
+                    Instead of trying to evaluate a high-dimensional filter over a two dimensional image, the algorithm presented 
+                    in <i>Adaptive Manifolds for Real-Time High-Dimensional Filtering</i> computes a series of manifolds, surfaces in high-dimensional space, where applying the 
+                    filter is equivalent to performing a Gaussian Blur over the surface of the manifold. Unlike images, which are flat surfaces, manifolds can be curved surfaces. 
+                    The manifolds are represented by a collection of points on its surface, one per a pixel in the original image. By taking into account the curvature of a 
+                    manifold, a Gaussian Blur can be approximated in a single loop over the points on the manifold. The blurred manifolds can then be sampled to obtain an 
+                    approximation of the filtered image.
+                </Typography>
+                <Space size='xs'/>
+                <Typography>
+                    QUALITY OF THE APPROXIMATION DEPENDS ON THE MANIFOLDS
+                </Typography>
+                <Space size='sm'/>
+                {/* Images of manifold tree */}
+                <Space size='md'/>
+                <Typography>
+                    {/* Explaination of blurring */}
+                </Typography>
+                <Space size='sm'/>
+                {/* Images of blurring */}
+                <Space size='md'/>
+                <Typography>
+                    {/* Explaination of denoising */}
+                </Typography>
+                <Space size='sm'/>
+                {/* Images of denoising */}
+                <Space size='md'/>
+                <Typography>
+                    {/* Explaination of depth of field */}
+                </Typography>
+                <Space size='sm'/>
+                {/* Images of depth of field */}
                 <Space size='lg'/>
                 <Suggestions labels={[
                         'Gastal et al',
